1e633000a9d62f4f11ce68173c8f9fc70617980c
spdbear <16793732+spdbear@users.noreply.github.com>
2021-04-02 22:46:01 +0900

    モックのレイアウトができた

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,14 +7,17 @@ function App() {
       <div className="chat-wrapper">
         <div className="message-wrapper">
           <div className="message">あ〜〜〜〜〜</div>
+          <div className="time">20:00</div>
         </div>
         <div className="my-message-wrapper">
           <div className="message">う〜〜〜</div>
+          <div className="time">20:00</div>
         </div>
         <div className="message-wrapper">
           <div className="message">
             <div>画像が入る</div>
           </div>
+          <div className="time">20:00</div>
         </div>
       </div>
     </div>
