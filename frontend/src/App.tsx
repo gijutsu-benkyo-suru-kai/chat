9e76e8c22cf4d63e8aa5583618c59df4b375036d
spdbear <16793732+spdbear@users.noreply.github.com>
2021-04-02 22:19:32 +0900

    モックを作成

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="chat-wrapper">
+        <div className="message-wrapper">
+          <div className="message">あ〜〜〜〜〜</div>
+        </div>
+        <div className="message-wrapper">
+          <div className="my-message">う〜〜〜</div>
+        </div>
+        <div className="message-wrapper">
+          <div className="message">
+            <div>画像が入る</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
