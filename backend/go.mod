974fed35cb2433200c6398842b4508f5f9c2bf83
spdbear <16793732+spdbear@users.noreply.github.com>
2021-04-17 22:42:35 +0900

    素のJSONを返すようにした

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/gijutsu-benkyo-suru-kai/chat
+
+go 1.16
---
+++ backend/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, `[
+  {
+    "userId": 2,
+    "userName": "たろう",
+    "content": "あ〜〜〜〜〜",
+    "time": 1617371835000
+  },
+  {
+    "userId": 1,
+    "userName": "じろう",
+    "content": "う〜〜〜",
+    "time": 1617372835000
+  },
+  {
+    "userId": 2,
+    "userName": "たろう",
+    "content": "画像が入る",
+    "time": 1617373835000
+  }
+]`)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
