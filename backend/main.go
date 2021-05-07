3e8c05322b3611a507da94aed9905429112dfb44
spdbear <16793732+spdbear@users.noreply.github.com>
2021-05-08 00:11:26 +0900

    DELETEメソッドを実装

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -71,6 +74,36 @@ func main() {
 			messageList = append(messageList, message)
 			log.Printf("%+v\n", messageList)
 		}
+	})
+	http.HandleFunc("/message/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "DELETE" {
+			w.WriteHeader(405)
+			return
+		}
+		// eq. '/message/3' -> '/3'
+		substr := strings.TrimPrefix(r.URL.Path, "/message")
+		_, id_str := filepath.Split(substr)
+		id, err := strconv.ParseUint(id_str, 10, 64)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(400)
+			return
+		}
+		found := false
+		for i, message := range messageList {
+			if message.Id == uint(id) {
+				// messageList[i] の要素を削除
+				messageList = append(messageList[:i], messageList[i+1:]...)
+				found = true
+				break
+			}
+		}
+		if !found {
+			w.WriteHeader(404)
+			return
+		}
+		w.WriteHeader(200)
+		return
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
